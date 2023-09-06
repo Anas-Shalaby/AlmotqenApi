f068f041589075481e7e350a269765df08a85643
Anas Youssef <anasyoussef649@gmail.com>
2023-09-06 11:44:23 +0300

    init

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,8 @@ export const MONGO_URI =
 export const JWT_SECRET = "Anas Shalaby";
 export const GMAIL = "anasyoussef649@gmail.com";
 export const GMAIL_PASS = "bgjnlysaqwgowyud";
-export let PROGRAM_END = 2;
 export const STRIPE_KEY =
   "sk_test_51LZXIrKczUq84uf3v5kIkkxRPy8RBy9BYmiqy6olmHpX9xSIllbzoQ8NjuzV9euHHzksgy2Bc6WT7tgiFaYHuCwq00wK07EYAW";
 
 export const SERVER_URL = "http://localhost:4000";
+export const PORT = process.env.PORT || 4000;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import App from "./services/ExpressApp";
 
 import dbConnection from "./services/Database";
+import { PORT } from "./config";
 
 const StartServer = async () => {
   const app = express();
@@ -10,7 +11,7 @@ const StartServer = async () => {
 
   await App(app);
 
-  app.listen(4000, () => {
+  app.listen(PORT, () => {
     console.clear();
     console.log("App is running to the port 4000");
   });
